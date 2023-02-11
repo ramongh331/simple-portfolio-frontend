761e6ce96eedd3218e853736cfea48b0d145c1ca
Ramon Hernandez <ramongh331@gmail.com>
2023-02-10 19:52:06 -0500

    added heroku link for backend to loader

--- src/Loaders.js
+++ src/Loaders.js
@@ -1,12 +1,7 @@
-const URL = "https://portfolio-api-cnfr.onrender.com"
+const URL = "https://ramon-portfolio-backend.herokuapp.com"
 
 export const projectsLoader = async () => {
     const response = await fetch(URL + "/projects");
-    const project = await response.json();
-    return project
-}
-export const projectLoader = async ({params}) => {
-    const response = await fetch(URL + "/projects/" + params.id);
     const project = await response.json();
     return project
 }
